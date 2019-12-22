1752d070bdebd89dff98e3cfa789293645d46fe2
Hamza Ahmad <toorhamza@gmail.com>
2019-12-23 00:02:15 +0200

    exercise 2.11 completed

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Form from './Form.js';
 import People from './People.js';
 import FilterSearch from "./FilterSearch.js";
+import axios from 'axios';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
   const [filterNames, setFilterNames] = useState(persons);
+
+  const fetchData = () => {
+    axios.get('http://localhost:3001/persons')
+    .then((data) => {
+        setPersons(data.data)
+        setFilterNames(data.data)
+    })
+}
+
+  useEffect(fetchData, [])
+
+
 
   const handleSubmit = e => {
     e.preventDefault();
